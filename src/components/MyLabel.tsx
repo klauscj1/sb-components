67c0d237f030f95e0d542e6a5faf3984590ffb24
Claus Chocho <clauschochoe@gmail.com>
2022-03-30 15:27:09 -0500

    gitignore updated

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+import "./MyLabel.css";
+
+interface Props {
+  /**
+   * Label to show
+   */
+  label: string;
+  /**
+   * Size of label
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Word on capital
+   */
+  allCaps: boolean;
+  /**
+   * Color of label
+   */
+  color: "primary" | "secondary" | "tertiary";
+  /**
+   * Color of font
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label = "no lable",
+  size = "normal",
+  color = "primary",
+  allCaps = false,
+  fontColor,
+}: Props) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{
+        color: fontColor,
+      }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
